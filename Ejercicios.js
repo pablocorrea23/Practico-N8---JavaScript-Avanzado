5c7e5a952d58628e7be220ba1daba17bb5ea79b3
joaquinquiroga22 <joaquinquiroga22@gmail>
2022-08-18 16:12:31 -0300

    first commit

---
+++ Ejercicios.js
@@ -0,0 +1,17 @@
+function convertirTexto() {
+    //Crear una funcion para generar el hash de un texto como numero entero
+    //Usando métodos ya establecidos
+    //Aqui tu codigo
+}
+
+
+function HashTable() {
+    //      Hash Table
+    //      Una hash table contiene un arreglo de "contenedores" donde puede guardar información.
+    //      Para guardar un valor asociado a una key (string):
+    //    - Correr la key a través de una función hash para transformarla a un número entero.
+    //    - Establecer el par llave/valor en tu tabla hash -- Usa un set
+    //    - Devuelve el valor almacenado en la tabla[índice] -- Usa un get
+    //      Aqui tu codigo
+
+}
